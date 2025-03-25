54fc00f89cab96befda523d589afa5c1dfe4d1db
Cody D'Ambrosio <cody.dambrosio@hpe.com>
2025-03-25 19:38:49 -0400

    fill in text field

--- scripts/content.js
+++ scripts/content.js
@@ -4,26 +4,45 @@
 
 const DEFAULT_ORBITVU_EXPORTS_DIR = "/default/orbitvu/exports/path"
 
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+// FIXME: Update these
+const ORBITVU_META_FILENAME = "test.txt"
+// Test at: https://www.roboform.com/filling-test-all-fields
+const SHOPIFY_SELECTOR = "input[type='text'][name='01___title']"
 
 async function import_orbitvu() {
+  // Prompt user for directory of orbitvu project
   const dirHandle = await window.showDirectoryPicker();
+
+  // get meta file
   let file = null;
   for await (const [key, value] of dirHandle.entries()) {
-    if (key == "test.txt") {
+    if (key == ORBITVU_META_FILENAME) {
       file = await value.getFile();
       break;
     }
   }
+
+  // read meta file
+  let fileContent = null;
   if (file) {
     console.log("Reading file", file);
     const reader = new FileReader();
     reader.onload = (e) => {
-      const fileContent = e.target.result;
-      console.log(fileContent);
+      fileContent = e.target.result;
     }
     reader.readAsText(file);
+    while (reader.readyState != FileReader.DONE) {
+      console.log("LOADING (",reader.readyState, ")");
+      await sleep(200);
+    }
+    console.log(fileContent);
   }
 
+  // file in text field in browser
+  let titleField = document.querySelector(SHOPIFY_SELECTOR)
+  titleField.value = fileContent;
 }
 
 
