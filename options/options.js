ee25385a1edd13b1338774609487ec93bf82ab23
Cody D'Ambrosio <cody.dambrosio@hpe.com>
2025-03-28 14:46:57 -0400

    style options menu

--- options/options.js
+++ options/options.js
@@ -1,20 +1,39 @@
+const STATUS_DISPLAY_TIME = 1500;
+
 // Saves options to chrome.storage
 const saveOptions = () => {
     const debug = document.getElementById('debug').checked;
     const orbitvu_meta_filename = document.getElementById('orbitvu_meta_filename').value;
+    const status = document.getElementById('status');
   
+    // Validate input
+    errMsg = null;
+    if (!orbitvu_meta_filename || !orbitvu_meta_filename.trim()) {
+      errMsg = "Metadata file can not be blank."
+    }
+  
+    if (errMsg) {
+      status.style.color = "red";
+      status.textContent = 'Error: ' + errMsg;
+      setTimeout(() => {
+        status.textContent = '';
+      }, STATUS_DISPLAY_TIME);
+      return;
+    }
+
+
     chrome.storage.sync.set(
       {
         debug: debug,
-        orbitvu_meta_filename: orbitvu_meta_filename
+        orbitvu_meta_filename: orbitvu_meta_filename.trim()
       },
       () => {
         // Update status to let user know options were saved.
-        const status = document.getElementById('status');
+        status.style.color = "black";
         status.textContent = 'Options saved.';
         setTimeout(() => {
           status.textContent = '';
-        }, 750);
+        }, STATUS_DISPLAY_TIME);
       }
     );
   };
