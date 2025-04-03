a62f43f30807a7062e22213115b8acfc1cf5161a
Cody D'Ambrosio <cody.dambrosio@hpe.com>
2025-04-03 14:50:15 -0400

    add broken code

--- src/content.js
+++ src/content.js
@@ -30,6 +30,7 @@ const SHOPIFY_SELECTORS = {
   // This is hidden, but is what is actually read and sent to backend
   DESCRIPTION: '#product-description',
   MEDIA: 'input[type="file"]',
+  CATEGORY: 'input[id="ProductCategoryPickerId"]',
   PRICE: 'input[name="price"]',
   COMPARE_AT_PRICE: 'input[name="compareAtPrice"]',
   COST_PER_ITEM: 'input[name="unitCost"]',
@@ -58,6 +59,43 @@ async function get_checkbox_element_by_label(label, selector = "span") {
     }
   }
   throw ReferenceError("Could not find checkbox with " + selector + " text '" + label + "'")
+}
+
+async function set_meta_field(label, value) {
+  await debugLog("set_meta_field(" + label + "," + value + ")")
+  // need to click to open input box
+  let selector = `div[role="button"][aria-label="Edit ${label} metafield"]`
+  let editBtn = document.querySelector(selector);
+  if (!editBtn) {
+    throw Error(`Could not find meta field ${label}: ${selector}`)
+  }
+  editBtn.click()
+  // find input field that should have popped up
+  let foundInputs = []
+  // give time for popover to appear
+  const timeout = Date.now() + 10000;
+  while (Date.now() < timeout) {
+    let popovers = document.querySelectorAll('div[data-portal-id*="cardPopover"]')
+    for (let i = 0; i < popovers.length; i++) {
+      input = popovers[i].querySelector("input")
+      if (input) {
+        foundInputs.push(input)
+      }
+    };
+    if (foundInputs.length > 0) {
+      break;
+    } else {
+      sleep(300);
+    }
+  }
+  if (foundInputs.length != 1) {
+    throw Error(`Could not find exactly 1 input for meta field ${label}: ${foundInputs.length}`)
+  }
+  // set input
+  Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set.call(foundInputs[0], value);
+  foundInputs[0].dispatchEvent(new Event("input", { bubbles: true}));
+  // click out
+  document.querySelector("body").click() 
 }
 
 async function file_to_json(file) {
@@ -138,6 +176,12 @@ async function import_orbitvu() {
   let barcode = json_data.session.barcode
 
 
+  const METADATA_MAP = {
+    "Inventory Location": "Warehouse Location"
+  }
+  let metadata = json_data.session.metadata
+
+
   /**
    * 
    * POPULATE SHOPIFY PAGE
@@ -157,10 +201,8 @@ async function import_orbitvu() {
     // Media Files
     await set_input_field(SHOPIFY_SELECTORS.MEDIA, mediaFiles.files, true);
   }
-  // Prices
-  //await set_input_field(SHOPIFY_SELECTORS.PRICE, "3.50")
-  //await set_input_field(SHOPIFY_SELECTORS.COMPARE_AT_PRICE, "4.20")
-  //await set_input_field(SHOPIFY_SELECTORS.COST_PER_ITEM, "0.69")
+  // TBD: Category
+  // TBD: Prices
   if (sku || barcode) {
     // SKU and barcode
     checkbox = await get_checkbox_element_by_label("This product has a SKU or barcode")
@@ -172,9 +214,22 @@ async function import_orbitvu() {
       await set_input_field(SHOPIFY_SELECTORS.BARCODE, barcode);
     }
   }
-  // weight
-  //await set_input_field(SHOPIFY_SELECTORS.WEIGHT, "20.5");
-  // TBD: weight unit...
+  // TBD: Weight
+
+  // meta fields
+  /* FIXME: THIS DOESN'T WORK
+  for(let i = 0; i < metadata.length; i++) {
+    let meta_name = metadata[i].name;
+    if (meta_name in METADATA_MAP) {
+      let meta_value = metadata[i].value;
+      let slabel = METADATA_MAP[meta_name]
+      await debugLog(`Found meta field '${meta_name}=${meta_value}'. Setting '${slabel}'`)
+      await set_meta_field(slabel, meta_value);
+    } else {
+      await debugLog(`Ignoring meta field '${meta_name}`)
+    }
+  }
+  */
 
   // Return to top
   document.querySelector(SHOPIFY_SELECTORS.APP_FRAME).scrollTo({ top: 0, behavior: 'smooth' });
