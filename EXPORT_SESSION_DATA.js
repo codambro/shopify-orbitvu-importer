84e7ac2ae1af57b0aee9ae5c5994992e7d4e9ec0
Cody D'Ambrosio <cody.dambrosio@hpe.com>
2025-04-03 10:17:54 -0400

    basic POC

--- EXPORT_SESSION_DATA.js
+++ EXPORT_SESSION_DATA.js
@@ -1,8 +1,17 @@
-// Usage: "On image publish" action
+// Usage: "On published start" action
 //
 // This script is copying session.json from workspace to export dir.
 //
 
+let sessionPath = Orbitvu.getSessionPath();
+let exportPath = Orbitvu.getExportFolder();
+let filename = "session.json"
+let sourceFile = sessionPath + "/" + filename;
+let destinationFile = exportPath + "/" + filename;
+
+Orbitvu.copyFile(sourceFile, destinationFile);
+
+/*
 let session = input["session"]
 let sessionDir = session["dir"]
 let sessionName = session["name"]
@@ -12,19 +21,15 @@ let idx = input["item"]["idx"]
 const exportt = input["export"]
 const expSuffix = exportt["fileSuffix"]
 let outFileSuffix = "." + expSuffix
-console.log(outFileSuffix)
+
+const rawName = "session.json"
+
+let rawFileLocation = sessionDir + "/" + sessionName
+let sessionFilename = rawFileLocation + "/" + rawName
 
 
-let sessionFilename = sessionDir + "/" + sessionName + "/session.json"
+rawExt = ".cr2"
 
-
-if (ov.isFile(rawPathMainCr2)===true){
-  console.log("CR2 raw")
-  rawExt = ".cr2"
-
-}
-
-let rawName = assetID + "_raw" + rawExt
 let finalOutputFile = outFile + rawExt
 finalOutputFile = finalOutputFile.replace(outFileSuffix,"")
 
@@ -34,11 +39,11 @@ let finalLocation = finalMainImage.substring(0,lastSlashIndex)
 
 
 let renamedMainRawLocation = finalLocation + "/" + rawName
-//console.log("CURRRENT finaloutput image   "  + finalOutputFile)
 let outRAW = outFile.replace(outFileSuffix, rawExt)
 
-//main photo
+
 if (ov.isFile(outRAW)===false){
   ov.copy(rawFileLocation,finalLocation,[rawName])
   ov.move(renamedMainRawLocation,outRAW)
 }
+*/
--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 Simple uploader for orbitvu project to Shopify
 
 ## Setup
+Various setup is needed to integrate Orbitvu with Shopify.
+
+### Orbitvu Station
+Within Orbitu Station:
+- Determine the location where 'actions' are stored. This can be found under Preferences > Saving > "Actions folder".
+![pref_saving](docs/images/pref_saving.png)
+- Copy `EXPORT_SESSION_DATA.js` into the actions folder.
+- Enable the action in Orbitu Station under Preferences > Actions > "On publish started".
+![pref_actions](docs/images/pref_actions.png)
+
+### Browser
 This is still in development, so extension needs to be ran in developer mode.
 
 - Clone repo locally, or download and extract ZIP.
@@ -11,7 +22,8 @@ This is still in development, so extension needs to be ran in developer mode.
 - Select the location to this repo (shopify-orbitvu-importer)
 
 ## Usage
-TBD
+From Shopify New Product page, click the extension icon to start importing.
+![ext_popup](docs/images/ext_popup.png)
 
 ## Todo List
 A lot still needs to be done...
